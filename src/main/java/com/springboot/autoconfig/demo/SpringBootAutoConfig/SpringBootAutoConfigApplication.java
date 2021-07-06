a430e3c0ab971814a629d3dc760bc54ab5ec38c5
Mohammed Shamsheer <shamsheer142089@gmail.com>
2021-07-06 06:12:40 +0530

    SPBOOT-01 Practice one sample

---
+++ src/main/java/com/springboot/autoconfig/demo/SpringBootAutoConfig/SpringBootAutoConfigApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.autoconfig.demo.SpringBootAutoConfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAutoConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAutoConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/autoconfig/demo/SpringBootAutoConfig/SpringBootAutoConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.autoconfig.demo.SpringBootAutoConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAutoConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
